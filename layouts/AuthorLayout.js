44ba756a4a6043818f4a66aaf478467b5746c5b7
Leo Huynh <huynhtuananh21895@gmail.com>
2021-11-05 22:34:48 +0700

    Update spotify APIs, update AuthorPage

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,46 +1,27 @@
-import SocialIcon from '@/components/social-icons'
-import Image from '@/components/Image'
+import ProfileCard from '@/components/ProfileCard'
 import { PageSEO } from '@/components/SEO'
 
-import useTranslation from 'next-translate/useTranslation'
-
 export default function AuthorLayout({ children, frontMatter, availableLocales }) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
-  const { t } = useTranslation()
+  const { name, avatar } = frontMatter
 
   return (
     <>
       <PageSEO
-        title={`${t('headerNavLinks:about')} - ${name}`}
-        description={`${t('SEO:about')} - ${name}`}
+        title={`About - ${name}`}
+        description={`About me - ${name}`}
         availableLocales={availableLocales}
       />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {t('headerNavLinks:about')}
+            About
           </h1>
         </div>
-        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8 space-x-2">
-            <Image
-              src={avatar}
-              alt="avatar"
-              width="192px"
-              height="192px"
-              className="w-48 h-48 rounded-full"
-            />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex pt-6 space-x-3">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:space-y-0 pt-8">
+          <ProfileCard avatar={avatar} />
+          <div className="pb-8 xl:pl-8 prose prose-lg dark:prose-dark max-w-none xl:col-span-2">
+            {children}
           </div>
-          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">{children}</div>
         </div>
       </div>
     </>
