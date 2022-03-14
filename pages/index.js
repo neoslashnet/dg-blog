9b2e6b58e69382a27214bd78243da09af3aed637
David Galiata | neoslash.net <david@neoslash.net>
2022-03-14 11:17:05 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,7 @@ export default function Home({ posts }) {
                 <Link href="/about" className="hover:underline">
                   <Twemoji emoji="face-with-monocle" /> More about me and myself
                 </Link>
+              </div>
               <p className="my-8">
                 Happy reading <Twemoji emoji="clinking-beer-mugs" />
               </p>
