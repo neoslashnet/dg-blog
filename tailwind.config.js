6c500eb8f08a1c63da279c6a0d9c5c73b365b44f
David Galiata | neoslash.net <david@neoslash.net>
2022-03-13 21:14:28 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['JetBrains Mono', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.indigo,
