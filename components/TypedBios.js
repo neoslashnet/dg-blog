c47974bbc9b1da8b7d51071ba8bc0a0e79e6d791
David Galiata | neoslash.net <david@neoslash.net>
2022-03-13 22:47:13 -0700

    Update TypedBios.js

--- components/TypedBios.js
+++ components/TypedBios.js
@@ -42,7 +42,7 @@ const TypedBios = () => {
         <li>
           I'm a sports-guy. I love
           <span className="ml-1">
-            <Twemoji emoji="soccer-ball" />
+          <Twemoji emoji="soccer-ball" />
           </span>
         </li>
         <li>I enjoy spending time with my family.</li>
