e4807699c93f0c98e49a63181794d3fccbe05c55
David Galiata | neoslash.net <david@neoslash.net>
2022-03-13 22:55:29 -0700

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
+      
       {
         components: ['Link'],
         specialLink: ['hrefLeft', 'hrefRight'],
