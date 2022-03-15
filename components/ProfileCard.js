e07583f7335f0c24780f408c268f3f70c75c5dd4
David Galiata | neoslash.net <david.galiata@aquia.io>
2022-03-14 18:08:30 -0700

    Update ProfileCard.js

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -6,7 +6,7 @@ import Twemoji from './Twemoji'
 import Link from 'next/link'
 
 const ProfileCard = () => {
-  //const { data } = useSWR('/api/spotify', fetcher)
+  const { data } = useSWR('/api/spotify', fetcher)
 
   const ref = useRef(null)
   const [style, setStyle] = useState({})
