5f36d694f1c808904822d82de2224142e0753092
David Galiata | neoslash.net <david.galiata@aquia.io>
2022-09-09 15:43:43 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "dl0ly56clg");
-</script>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/hat.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/hat.png" />
           <link rel="icon" type="image/jpg" sizes="16x16" href="/static/favicons/hat.jpg" />
